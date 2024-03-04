bc4147e2d503e6c8792ede15cf0bbcb08fff6869
mmirazz <mehedimiraz900@gmail.com>
2024-03-04 10:40:20 +0600

    handlePostTitle function added

--- script/index.js
+++ script/index.js
@@ -1,25 +1,47 @@
-const loadForum = async () => {
+const handlePostTitle = (title, views) => {
+  const readAs = document.getElementById("read-as");
+  
 
-  document.getElementById('loading-spiner').classList.remove('hidden')
-  const res = await fetch(
-    `https://openapi.programming-hero.com/api/retro-forum/posts`
-  );
-  const data = await res.json();
-  const postId =data.posts
+  const titleCount = document.getElementById("title-count");
+  const div = document.createElement("div");
+  div.innerHTML = `<div class="flex my-3 justify-between"><h2>${title}</h2>
+  <p><i class="fa-regular fa-eye"></i> ${views}</p></div>`;
 
-  const forumContainer = document.getElementById("forum-container");
-  postId.forEach((post) => {
-    document.getElementById('loading-spiner').classList.add('hidden')
-    const div = document.createElement("div");
-    div.innerHTML = `<div
+  titleCount.appendChild(div);
+};
+
+const handelSearch = () => {
+  const value = document.getElementById("search-box").value;
+
+  console.log(value);
+
+  if (value) {
+    loadForum(value);
+  } else {
+    alert("please enter valid text");
+  }
+};
+
+fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
+  .then((res) => res.json())
+  .then((data) => {
+    const forumContainer = document.getElementById("forum-container");
+    data.posts.forEach((item) => {
+      const post = item;
+
+      const div = document.createElement("div");
+      div.innerHTML = `<div
         class="mb-7 w-full lg:w-[90%]"
       >
         <div class="flex gap-4 lg:gap-20  bg-[#12132D0D]  rounded-xl p-10 ">
         <div class="indicator">
-        <span class="indicator-item badge ${ post.isActive ? "bg-green-500" : "bg-red-500" }"></span>
-        <div class="grid w-32 h-32 bg-base-300 place-items-center"><img src="${post.image}" alt=""></div>
+        <span class="indicator-item badge ${
+          post.isActive ? "bg-green-500" : "bg-red-500"
+        }"></span>
+        <div class="grid w-32 h-32 bg-base-300 place-items-center"><img src="${
+          post.image
+        }" alt=""></div>
       </div>
-        
           <div>
             <div class="flex justify-evenly mb-6">
               <p>#${post.category}</p>
@@ -32,13 +54,19 @@ const loadForum = async () => {
             <hr />
             <div class="flex  justify-between  mt-6">
               <div class="flex gap-7 ">
-                <p><i class="  fa-solid fa-message"></i> ${post.comment_count}</p>
+                <p><i class="  fa-solid fa-message"></i> ${
+                  post.comment_count
+                }</p>
                 <p><i class="  fa-solid fa-eye"></i>${post.view_count}</p>
-                <p><i class="  fa-regular fa-clock"></i> ${post.posted_time} min</p>
+                <p><i class="  fa-regular fa-clock"></i> ${
+                  post.posted_time
+                } min</p>
               </div>
               <div>
                 <p class="bg-[#10B981] rounded-full p-1">
-                  <button onclick="getId(${post.title})" ><i class="fa-solid fa-envelope-open"></i></button>
+                <button onclick="handlePostTitle('${post.title}', ${
+        post.view_count
+      })" ><i class="fa-solid fa-envelope-open"></i></button>
                 </p>
               </div>
             </div>
@@ -46,86 +74,45 @@ const loadForum = async () => {
         </div>
       </div>`;
 
-    forumContainer.appendChild(div);
+      document.getElementById("loading-spinner").classList.add("hidden");
+      // document.getElementById("loading-spinner2").classList.add("hidden");
+      forumContainer.appendChild(div);
+    });
   });
-};
 
+fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`)
+  .then((res) => res.json())
+  .then((data) => {
+    const postContainer = document.getElementById("post-container");
+    data.forEach((item) => {
+      // document.getElementById("loading-spinner2").classList.add("hidden");
+      const div = document.createElement("div");
 
-const getId = (postId) => {
-  const  titleCount = document.getElementById('title-count')
-  const div = document.createElement("div");
-  div.innerHTML =`
-  <h2>a</h2>
-  <p>1</p>`
+      div.innerHTML = `<div class="card w-96 h-full bg-base-100 shadow-xl">
+        <figure class="px-10 pt-10">
+          <img src="${item.cover_image}" alt="" class="rounded-xl" />
+        </figure>
+        
+        <div class="card-body ">
+          <p> <span class="mr-2"><i class="fa-solid fa-briefcase"></i></span>${
+            item?.author?.posted_date || "No publish date"
+          }</p>
+          <h2 class="card-title">${item.title}</h2>
+          <p>${item.description}</p>
+          <div class="flex gap-6">
 
- titleCount.appendChild(div)
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-const postLoader = async () => {
-
-  document.getElementById('loading-spiner2').classList.remove('hidden')
-  const res = await fetch(
-    `https://openapi.programming-hero.com/api/retro-forum/latest-posts`
-  );
-  const data = await res.json();
-  const postContainer = document.getElementById("post-container");
-  data.forEach((item) => {
-    document.getElementById('loading-spiner2').classList.add('hidden')
-    const div = document.createElement("div");
-
-    div.innerHTML = `    <div class="card w-96 bg-base-100 shadow-xl">
-  <figure class="px-10 pt-10">
-    <img src="${item.cover_image}" alt="" class="rounded-xl" />
-  </figure>
-  
-  <div class="card-body ">
-    <p> <span class="mr-2"><i class="fa-solid fa-briefcase"></i></span>${item?.posted_date}</p>
-    <h2 class="card-title">${item.title}</h2>
-    <p>${item.description}</p>
-    <div class="flex gap-6">
-
-      <div class="w-16 "><img class="rounded-full" src="${item.profile_image}" alt=""></div>
-      <div>
-        <h4>${item.author.name}</h4>
-        <p>${item.author?.designation}</p>
+            <div class="w-16 "><img class="rounded-full" src="${
+              item.profile_image
+            }" alt=""></div>
+            <div>
+              <h4>${item.author.name}</h4>
+              <p>${item?.author?.designation || "Unknown"}</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>`;
-
-    postContainer.appendChild(div);
+      `;
+      document.getElementById("loading-spinner2").classList.add("hidden");
+      postContainer.appendChild(div);
+    });
   });
-};
-
-
-
-
-const handelSearch=()=>{
-  const value = document.getElementById('search-box').value
-
-  console.log(value)
-
-  if(value){
-    loadForum(value)
-  }
-  else{
-    alert('please enter valid text')
-  }
-
-}
-
-postLoader();
-
-loadForum();
